edc15e0a51d991c791db7492405578201df36c73
Paweł Rubach <pawel.rubach@sorcersoft.com>
2020-03-23 14:45:16 +0100

    Comparing Values - strings

--- src/pl/waw/sgh/ComparingValues.java
+++ src/pl/waw/sgh/ComparingValues.java
@@ -25,5 +25,9 @@ public class ComparingValues {
         if (s1==s2) {
             System.out.println("s1 equal s2");
         }
+
+        if (s1.equals(s2)) {
+            System.out.println("s1 equal s2");
+        }
     }
 }
