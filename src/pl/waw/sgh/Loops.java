560f4187cef413a0d00ab665b0b5289d2ab2d648
Paweł Rubach <pawel.rubach@sorcersoft.com>
2020-03-23 14:52:05 +0100

    Loops

---
+++ src/pl/waw/sgh/Loops.java
@@ -0,0 +1,16 @@
+package pl.waw.sgh;
+
+public class Loops {
+
+    public static void main(String[] args) {
+        for (int i=0;i<5;i++) {
+            System.out.println("i=" + i);
+        }
+
+        for (int j=10;j>2;j--) {
+            System.out.println("j=" + j);
+        }
+
+
+    }
+}
