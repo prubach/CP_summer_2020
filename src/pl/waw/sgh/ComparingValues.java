dc03ee006eadf81c3d833bccdcd0f66116e2bda6
Paweł Rubach <pawel.rubach@sorcersoft.com>
2020-03-23 14:35:38 +0100

    Comparing Values - strings

---
+++ src/pl/waw/sgh/ComparingValues.java
@@ -0,0 +1,29 @@
+package pl.waw.sgh;
+
+public class ComparingValues {
+
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 8;
+
+        if (a==b) {
+            System.out.println("a equal b");
+        }
+
+        Integer aa = 8;
+        Integer bb = 8;
+
+        if (aa==bb) {
+            System.out.println("aa equal bb");
+        }
+
+        String s1 = "abc";
+        String s2 = new String("abc");
+        System.out.println(s1);
+        System.out.println(s2);
+
+        if (s1==s2) {
+            System.out.println("s1 equal s2");
+        }
+    }
+}
