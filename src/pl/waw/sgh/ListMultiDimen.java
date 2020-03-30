2a2cda912e588bbe97ed2d7f5af227e8fe3d3627
Paweł Rubach <pawel.rubach@sorcersoft.com>
2020-03-30 16:41:34 +0200

    Sum rows and columns of an 2D array

--- src/pl/waw/sgh/ListMultiDimen.java
+++ src/pl/waw/sgh/ListMultiDimen.java
@@ -5,6 +5,34 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ListMultiDimen {
+
+    public static int totalSum(List<List<Integer>> matrix) {
+        int sum = 0;
+        for (List<Integer> row : matrix) {
+            for (Integer elem : row) {
+                sum += elem; // sum = sum + elem
+            }
+        }
+        return sum;
+    }
+
+    public static int sumRow(List<List<Integer>> matrix, int rowIndex) {
+        int sum = 0;
+        List<Integer> row = matrix.get(rowIndex);
+        for (Integer elem : row) {
+            sum += elem; // sum = sum + elem
+        }
+        return sum;
+    }
+
+    public static int sumColumn(List<List<Integer>> matrix, int colIndex) {
+        int sum = 0;
+        for (List<Integer> row : matrix) {
+            sum += row.get(colIndex);
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         List<List<Integer>> myList2d = new ArrayList<>();
         List<Integer> row = Arrays.asList(new Integer[]{46, 25, 466});
@@ -17,8 +45,10 @@ public class ListMultiDimen {
             System.out.println(Arrays.toString(arr));
         }
         // 1. Total sum of all elements
+        System.out.println("Total sum: " + totalSum(myList2d));
         // 2. Sum of each row
+        System.out.println("Sum row 0: " + sumRow(myList2d, 0));
         // 3. Sum of each column !!!
-
+        System.out.println("Sum col 1: " + sumColumn(myList2d, 1));
     }
 }
