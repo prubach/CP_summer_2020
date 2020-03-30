c1664de929c7ca0a7f788ffb3c2a9dfdc42dbff3
Paweł Rubach <pawel.rubach@sorcersoft.com>
2020-03-30 17:21:44 +0200

    Reading files

---
+++ src/pl/waw/sgh/FileReading.java
@@ -0,0 +1,23 @@
+package pl.waw.sgh;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileReading {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String fileName = "test.txt";
+        String cwd = System.getProperty("user.dir");
+        System.out.println(cwd);
+
+        File myFile = new File(fileName);
+        Scanner scanner = new Scanner(myFile);
+        int i = 0;
+        while (scanner.hasNextLine()) {
+            String myLine = scanner.nextLine();
+            i++;
+            System.out.println("Line " + i + " : " + myLine);
+        }
+    }
+}
