94af53524f549a8819486079bff8baef2b42ad6c
Paweł Rubach <pawel.rubach@sorcersoft.com>
2020-04-06 16:44:27 +0200

    Recursion

--- src/pl/waw/sgh/BowlingRecursion.java
+++ src/pl/waw/sgh/BowlingRecursion.java
@@ -1,18 +1,16 @@
 package pl.waw.sgh;
 
 //
-public class Bowling {
+public class BowlingRecursion {
     static int bowling(int n) {
-        int sum = 0;
-        for(int i = 0; i <= n; i++) {
-            sum += i;
-        }
-        return sum;
+        if (n==1) return 1;
+        else return bowling(n-1) + n;
     }
+
     //verification
     public static void main(String[] args) {
         //System.out.println(bowling(5));
         //System.out.println(bowling(3));
-        System.out.println(bowling(34683));
+        System.out.println(bowling(3468));
     }
 }
--- src/pl/waw/sgh/FileOperations.java
+++ src/pl/waw/sgh/FileOperations.java
@@ -1,13 +1,14 @@
 package pl.waw.sgh;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileOperations {
 
-    public static void main(String[] args) {
-        //String filePath = "src/pl/waw/sgh/";
+    public static void main(String[] args) throws IOException {
+        String filePath = "src/pl/waw/sgh/";
         //String filePath = ".gitignore";
-        String filePath = "C:/pagefile.sys";
+        //String filePath = "C:/pagefile.sys";
         //String filePath = "C:\\pagefile.sys";
         // "\n"
         // "\t"
@@ -20,6 +21,24 @@ public class FileOperations {
         System.out.println("Is Directory: " + myFile.isDirectory());
         System.out.println("Is File: " + myFile.isFile());
         System.out.println("Is Hidden: " + myFile.isHidden());
+        System.out.println("Length: " + myFile.length());
+        System.out.println("Absolute path: " + myFile.getAbsolutePath());
+        //myFile.can
+
+        System.out.println("Free space: " + myFile.getFreeSpace());
+        System.out.println("Parent: " + myFile.getParentFile().getAbsolutePath());
+
+        //File[] files = myFile.listFiles();
+        //File[] files = myFile.listFiles((dir, name) -> name.startsWith("File"));
+        File[] files = myFile.listFiles((dir, name) -> new File(dir + "/" + name).length()>1200);
+
+        for (File f : files) {
+            System.out.println(f.getCanonicalPath());
+            System.out.println("Is Directory: " + f.isDirectory());
+            System.out.println("Length: " + f.length());
+        }
+
+
 
     }
 }
