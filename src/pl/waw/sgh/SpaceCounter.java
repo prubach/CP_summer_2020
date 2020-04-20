a2ecb9b50ac08652ce5398c3d0a7a860659ebd79
Paweł Rubach <pawel.rubach@sorcersoft.com>
2020-04-20 13:50:20 +0200

    Spaced occupied by a folder

---
+++ src/pl/waw/sgh/SpaceCounter.java
@@ -0,0 +1,22 @@
+package pl.waw.sgh;
+
+import java.io.File;
+
+public class SpaceCounter {
+
+    public static int occupiedSpace(File directory) {
+        int spaceOccup = 0;
+        // Your code
+
+        return spaceOccup;
+    }
+
+
+    public static void main(String[] args) {
+        //
+        File dir = new File("src/pl/waw/sgh");
+
+        System.out.println(occupiedSpace(dir));
+    }
+
+}
